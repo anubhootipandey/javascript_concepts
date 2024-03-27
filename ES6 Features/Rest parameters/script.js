aa58a52595dc2d445345b9426c10224ab26cc842
anubhootipandey <anubhootipandey05@gmail.com>
2024-03-27 13:59:09 +0530

    rest-parameters

--- ES6 Features/Rest parameters/script.js
+++ ES6 Features/Rest parameters/script.js
@@ -3,11 +3,37 @@
 
 const nums = [1, 2, 3, 4]
 
+// function add(...num){
+//     console.log(num);
+//     let sum = 0
+//     for(let i=0; i<num.length; i++){
+//         sum += num[i]
+//     }
+//     return sum
+// }
+
+// function add(a, b, c, ...num){ //rest parameter always used at the last formal parameter.
+//     console.log(a, b, c);
+//     console.log(num);
+//     // let sum = 0
+//     // for(let i=0; i<num.length; i++){
+//     //     sum += num[i]
+//     // }
+//     // return sum
+// }
+
+// // const res = add(...nums, 8, 90)
+// const res = add(8, 90)
+
+// function add(){
+//     return [...arguments].reduce((acc, curr) => acc + curr)
+// }
+// function add(){
+//     return Array.from(arguments).reduce((acc, curr) => acc + curr)
+// }
+
 function add(...num){
-    console.log(num);
-    let sum = 0
-    for(let i=0; i<num.length; i++){
-        sum += num[i]
-    }
-    return sum
+    return num.reduce((acc, curr) => acc + curr)
 }
+
+const result = add(...nums)
