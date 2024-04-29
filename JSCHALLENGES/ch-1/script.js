c541fe7169cde783c03e3e5db19d16125a37aeaf
anubhootipandey <anubhootipandey05@gmail.com>
2024-04-29 08:28:44 +0530

    javacsript challenges

---
+++ JSCHALLENGES/ch-1/script.js
@@ -0,0 +1,14 @@
+//Challenge 1
+console.log("Hello, World!");
+
+//Challenge-2
+for(let i=1; i<=5; i++){
+    console.log(i);
+}
+
+//Challenge-3
+function addTwoNumber(a, b){
+    return a + b;
+}
+console.log(addTwoNumber(4, 5));
+
