10cb3f84b61217bfebb9aa1b470b39bb9d0256ea
anubhootipandey <anubhootipandey05@gmail.com>
2024-05-01 21:39:28 +0530

    interview question

--- JSCHALLENGES/ch-1/script.js
+++ JSCHALLENGES/ch-1/script.js
@@ -12,3 +12,17 @@ function addTwoNumber(a, b){
 }
 console.log(addTwoNumber(4, 5));
 
+//Challenge-4
+function areaOfRectangle(length, breadth){
+    return length * breadth;
+}
+console.log(areaOfRectangle(7, 8));
+
+//Challenge-5
+function reverseString(){
+    let a, b, temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+console.log(reverseString());
