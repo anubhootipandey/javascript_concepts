c267ab12458443badac50d88da1f7c79b4b12457
anubhootipandey <anubhootipandey05@gmail.com>
2024-05-03 18:22:22 +0530

    notes of set and map

--- JSCHALLENGES/ch-4/script.js
+++ JSCHALLENGES/ch-4/script.js
@@ -8,5 +8,14 @@ mySet.add('value1');
 mySet.add('value2');
 console.log(mySet);
 
-//Map:- used to store key-value pairs where each key is unique.
-//Set:- used to store unique values of any type, where each value is unique.
+/*Map:- 
+    --used to store key-value pairs where each key is unique.
+    --suitable for scenarios where you need to associate data with unique keys and perform operations such as adding, 
+       removing, and updating key-value pairs.
+*/
+      
+/*Set:- 
+    --used to store unique values of any type, where each value is unique.
+    --useful when you need to store a collection of unique values and perform operations such as adding, removing, and
+      checking for the presence of values.
+*/
