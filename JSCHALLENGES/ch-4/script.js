a2f5b7b25465676dddf18dffaf65f70170acc05f
anubhootipandey <anubhootipandey05@gmail.com>
2024-05-03 18:16:45 +0530

    map vs set

---
+++ JSCHALLENGES/ch-4/script.js
@@ -0,0 +1,10 @@
+const myMap = new Map();
+myMap.set('key1', 'value1');
+myMap.set('key2', 'value2');
+console.log(myMap);
+
+const mySet = new Set();
+mySet.add('value1');
+mySet.add('value2');
+console.log(mySet);
+
