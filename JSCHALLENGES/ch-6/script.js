d3127372cc538ebaa4841e29e9c780e8fd023f03
anubhootipandey <anubhootipandey05@gmail.com>
2024-05-05 16:07:48 +0530

    jschallenges6

--- JSCHALLENGES/ch-6/script.js
+++ JSCHALLENGES/ch-6/script.js
@@ -1,10 +1,23 @@
-function myFunction() {
+// function myFunction() {
+//     let person = prompt("Please enter your name", "Harry Potter");
+//     if (person != null) {
+//       document.getElementById("demo").innerHTML =
+//       "Hello " + person + "! How are you today?";
+//     }
+//   }
+// console.log(myFunction());
+
+
+function myFunction(){
     let person = prompt("Please enter your name", "Harry Potter");
-    if (person != null) {
-      document.getElementById("demo").innerHTML =
-      "Hello " + person + "! How are you today?";
-    }
+  if (person != null) {
+    document.getElementById("demo").innerHTML =
+    "Hello " + person + "! How are you today?";
   }
-console.log(myFunction());
-
-
+  else {
+    window.stop()
+    document.getElementById("demo").innerHTML =
+    "Hello " + person + "! How are you today?";
+    }
+}
+myFunction()
