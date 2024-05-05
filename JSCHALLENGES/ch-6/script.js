ac6f96ae2b878822e721c99226e014b3346f028a
anubhootipandey <anubhootipandey05@gmail.com>
2024-05-05 16:07:26 +0530

    jschallenges6

---
+++ JSCHALLENGES/ch-6/script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    let person = prompt("Please enter your name", "Harry Potter");
+    if (person != null) {
+      document.getElementById("demo").innerHTML =
+      "Hello " + person + "! How are you today?";
+    }
+  }
+console.log(myFunction());
+
+
