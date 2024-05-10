00ae6f794aaced58dd8ed79520537068a1f8e6ab
anubhootipandey <anubhootipandey05@gmail.com>
2024-05-10 23:59:58 +0530

    strict operator

--- Linkedin-Js_Challenges/02ch/script.js
+++ Linkedin-Js_Challenges/02ch/script.js
@@ -1,11 +1,22 @@
-function Person(firstName, lastName){
-    this.firstName = firstName;
-    this.lastName = lastName;
+// function Person(firstName, lastName){
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+// }
+
+// var anu = new Person('Anu', 'Pandey');
+// var tanu = new Person('Tanu', 'Pandey');
+// var satyabrat = new Person('Satyabrat', 'Pandey');
+// var nidhi = new Person('Nidhi', 'Pandey');
+
+// console.table([anu, tanu, satyabrat, nidhi]);
+
+
+function data(){
+    let a = 3;
+    let b = new Number(3);
+
+    console.log(a == b);
+    console.log(a === b);
 }
 
-var anu = new Person('Anu', 'Pandey');
-var tanu = new Person('Tanu', 'Pandey');
-var satyabrat = new Person('Satyabrat', 'Pandey');
-var nidhi = new Person('Nidhi', 'Pandey');
-
-console.table([anu, tanu, satyabrat, nidhi]);
+data();
