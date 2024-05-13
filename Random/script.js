50217d9bec10f8d48aa28840a5e8cb442ae65140
anubhootipandey <anubhootipandey05@gmail.com>
2024-05-13 20:45:24 +0530

    coding question

---
+++ Random/script.js
@@ -0,0 +1,63 @@
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+console.log(myStr);
+
+/*------------------------------------------------------------------------------------------------------*/
+
+var firstNameLength = 0;
+var firstName = "Ada";
+firstNameLength = firstName.length;
+console.log(firstNameLength);
+
+/*------------------------------------------------------------------------------------------------------*/
+
+var myString = "Hello";
+myString = "Hii";
+console.log(myString);
+
+/*------------------------------------------------------------------------------------------------------*/
+
+// const ourString = "Hello";
+// ourString = "Hii";
+// console.log(ourString);
+
+/*------------------------------------------------------------------------------------------------------*/
+
+//bracket notation to find nth character in string
+var myFirstName = "Ada";
+var secondLetterOfFirstName = myFirstName[2];
+console.log(secondLetterOfFirstName);
+
+/*------------------------------------------------------------------------------------------------------*/
+
+//bracket notation to find last character in string
+var myPehlaName = "Ada";
+var lastLetterOfFirstName = myPehlaName[myPehlaName.length - 1];
+console.log(lastLetterOfFirstName);
+
+/*------------------------------------------------------------------------------------------------------*/
+
+//bracket notation to find nth to last character in string
+var pehlaName = "Anubhooti";
+var thirdToLastLetterOfFirstName = pehlaName[pehlaName.length - 3];
+console.log(thirdToLastLetterOfFirstName);
+
+/*------------------------------------------------------------------------------------------------------*/
+
+
+
+/*------------------------------------------------------------------------------------------------------*/
+
+
+
+/*------------------------------------------------------------------------------------------------------*/
+
+
+
+/*------------------------------------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------------------------------*/
+
+
+
+/*------------------------------------------------------------------------------------------------------*/
--- innerText and textContent/script.js
+++ innerText and textContent/script.js
@@ -3,3 +3,4 @@ const paragraph = document.querySelector('p')
 //console.log(paragraph.innerHTML)
 
 // paragraph.innerHTML = '<p>Hiii</p>'
+
