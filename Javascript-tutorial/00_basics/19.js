33de1c7efcfafa358f7ad8ebde05c2e4c8cfcc04
anubhootipandey <anubhootipandey05@gmail.com>
2024-05-14 23:46:42 +0530

    call, arrow function, this keyword

--- Javascript-tutorial/00_basics/19.js
+++ Javascript-tutorial/00_basics/19.js
@@ -9,19 +9,18 @@ to call directly, and it executes the function with those
 arguments.
 */
 
-function about(hobby, favSinger){
-    console.log(this.firstName, this.age, hobby, favSinger);
-}
-
 const user1 = {
     firstName: "Satyabrat",
     age: 15,
+    about: function(){
+        console.log(this.firstName, this.age);
+    }
 }
 
-const user2 = {
-    firstName: "Tanu",
-    age: 19, 
-}
+// const user2 = {
+//     firstName: "Tanu",
+//     age: 19, 
+// }
 
 // user1.about.call(user2, "sleep", "Mohit Chauhan")
 
@@ -45,7 +44,44 @@ making a copy of a function but with a predetermined context
 (or value of this). You can execute this new function later.
 */
 
-const func = about.bind(user2, "guitar", "Shreya");
-func();
+// const func = about.bind(user2, "guitar", "Shreya");
+// func();
 
+
+//---------------------------------------------------------
+//--------------------Don't do this mistake----------------
+//---------------------------------------------------------
+
+//user1.about();
+// const myFunc = user1.about;//undefined
+// myFunc();
+const myFunc = user1.about.bind(user1);
+myFunc();
+
+
+//------------------------------arrow function------>
+
+const user3 = {
+    firstName: "Tanu",
+    age: 19,
+    about: () => {
+        console.log(this);
+        console.log(this.firstName, this.age);
+    }
+}
+user3.about.call(user3);//Since user3.about is an arrow 
+//function, its this refers to the this of the surrounding 
+//scope where it was defined, not to the user3 object.
+//So, when you call user3.about.call(user3), it doesn't 
+//change the value of this inside the arrow function because 
+//arrow functions are not affected by call, apply, or bind. 
+//Therefore, this.firstName and this.age inside the arrow 
+//function about still refer to the this of the surrounding 
+//lexical context, which in this case is likely the global 
+//object (or undefined in strict mode).
+
+
+
+//Arrow functions don't bind their own this, instead, 
+//they inherit this from the surrounding lexical context.
 
