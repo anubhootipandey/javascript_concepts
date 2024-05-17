c0d986d5694f05518c9fa4010e7c39100cdef34f
anubhootipandey <anubhootipandey05@gmail.com>
2024-05-17 14:37:45 +0530

    ajax, sendRequest, onload, onerror, open, XMLHttpRequest(), error handling, fetch, promises

---
+++ Javascript-tutorial/04_Advanced/ajax.js
@@ -0,0 +1,28 @@
+const URL = "https://jsonplaceholder.typicode.com/posts";
+
+const xhr = new XMLHttpRequest();
+// console.log(xhr);
+
+//step: 1
+// console.log(xhr.readyState);
+xhr.open("GET", URL);
+// console.log(xhr.readyState);
+// xhr.onreadystatechange = function(){
+//     // console.log(xhr.readyState);
+//     if(xhr.readyState === 4){
+//         // console.log(xhr.readyState);
+//         // console.log(typeof xhr.response);
+//         const response = xhr.response;
+//         const data = JSON.parse(response);
+//         console.log(typeof data);
+//     }
+// }
+
+xhr.onload = function(){
+    // console.log(xhr.readyState);
+    const response = xhr.response;
+    const data = JSON.parse(response);
+    console.log(data);
+}
+
+xhr.send();
