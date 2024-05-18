8ccbdc544d8d962f497005e87d51cfb38dff4e26
anubhootipandey <anubhootipandey05@gmail.com>
2024-05-18 23:13:58 +0530

    Math Sequence Challenge 2

--- 001_JS_CHALLENGES/mathSequences.js
+++ 001_JS_CHALLENGES/mathSequences.js
@@ -1,23 +1,56 @@
+// function mathSequence(arr){
+//     let arithmetic = new Set();
+//     let geometric = new Set();
+
+//     for(let i=1; i<arr.length; i++){
+//         let arith = arr[i] - arr[i - 1];
+//         arithmetic.add(arith);
+//         let geo = arr[i] / arr[i - 1];
+//         geometric.add(geo);
+//     }
+
+//     if(arithmetic.size === 1) {
+//         return "Arithmetic";
+//     }
+//     if(geometric.size === 1) {
+//         return "Geometric";
+//     }
+//     return -1;
+// }
+
+// console.log(mathSequence([2, 4, 6, 8]));
+// console.log(mathSequence([3, 9, 27]));
+// console.log(mathSequence([2, 3, 5, 9]));
+
+
+
+//---------------------------OR------------------------------>
+
 function mathSequence(arr){
-    let arithmetic = new Set();
-    let geometric = new Set();
-
-    for(let i=1; i<arr.length; i++){
-        let arith = arr[i] - arr[i - 1];
-        arithmetic.add(arith);
-        let geo = arr[i] / arr[i - 1];
-        geometric.add(geo);
-    }
-
-    if(arithmetic.size === 1) {
-        return "Arithmetic";
-    }
-    if(geometric.size === 1) {
-        return "Geometric";
-    }
-    return -1;
+	let returnValue = "";
+	for(let i = 0; i < arr.length-1; i++){
+		if(arr[i] + arr[0] === arr[i+1]){
+			returnValue = "Arithmetic"
+		}
+		else if(arr[i] * arr[0] === arr[i+1]){
+			returnValue = "Geometric"
+		}
+		else{
+			returnValue = "No Pattern"
+		}
+	}
+	return(returnValue)	
 }
 
 console.log(mathSequence([2, 4, 6, 8]));
 console.log(mathSequence([3, 9, 27]));
 console.log(mathSequence([2, 3, 5, 9]));
+
+
+
+
+
+
+
+
+
