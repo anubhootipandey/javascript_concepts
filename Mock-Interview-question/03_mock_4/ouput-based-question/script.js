bdc994353a9d1b572220d43d90859409f4350dd0
anubhootipandey <anubhootipandey05@gmail.com>
2024-05-21 00:04:42 +0530

    output based question

---
+++ Mock-Interview-question/03_mock_4/ouput-based-question/script.js
@@ -0,0 +1,21 @@
+//Q--1
+// console.log(typeof ('5' + 3));
+// console.log(typeof ('5' - 3));
+
+//Q--2
+// console.log('2' + 3 * 4);
+
+//Q--3
+// var a = 5;
+// console.log(a++);
+// console.log(a);
+
+//Q--4
+// console.log(typeof NaN);
+
+//Q-5
+// console.log(NaN === NaN);
+
+//Q-6
+console.log(1 < 2 < 3);
+console.log(3 > 2 > 1);
