1bfdabc73000331721f22f7f2c0ab9117c3d50af
anubhootipandey <anubhootipandey05@gmail.com>
2024-06-18 20:13:09 +0530

    onclick dom manipulation

---
+++ Interview-Question/coding-question/02_code_0/script.js
@@ -0,0 +1,7 @@
+//Task: onclicking div show the text content in console
+
+const result = document.getElementById('result');
+
+result.addEventListener("click", () => {
+    console.log('Welcome Anubhooti!');
+});
