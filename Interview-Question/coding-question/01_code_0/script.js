c4bc4f9d12a93748daf2c4739e5e121e05e5fd66
anubhootipandey <anubhootipandey05@gmail.com>
2024-06-18 20:07:50 +0530

    map filter and forEach

--- Interview-Question/coding-question/01_code_0/script.js
+++ Interview-Question/coding-question/01_code_0/script.js
@@ -17,10 +17,34 @@ let users = [
     },
 ];
 
+const result = document.getElementById("result");
+
 const ans = users.filter(user => {
     return user.age < 28;
-})
+});
 
 console.log(ans);
+
+// By creating new div
+function displayUsers(users){
+    users.forEach(user => {
+        const userDiv = document.createElement("div");
+        userDiv.innerHTML = `Name:</strong> ${user.name} <br> Name:</strong> ${user.age}`;
+        result.appendChild(userDiv);
+    });
+}
+
+
+
+//if we do this way then it will only show one data not multiple data if present
+// function displayUsers(users){
+//     result.innerHTML = "";
+    
+//     users.forEach(user => {
+//         result.innerHTML = `<p>Name: ${user.name} <br> Age: ${user.age}`;
+//     });
+// }
+
+displayUsers(ans);
 
 
