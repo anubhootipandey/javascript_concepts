236bcdc2fa216afe549d44b23edc55db95003a68
anubhootipandey <anubhootipandey05@gmail.com>
2024-06-24 14:02:04 +0530

    evnt propagation

--- MyLearning/Javascript/EventCapturing/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("#div1").addEventListener("click", () => {
-    console.log("Div 1");
-}, true);
-document.querySelector("#div2").addEventListener("click", () => {
-    console.log("Div 2");
-}, true);
-document.querySelector("#div3").addEventListener("click", () => {
-    console.log("Div 3");
-}, true);
