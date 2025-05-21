df4ee726039609461abc01e48a99b807b3e8a371
anubhootipandey <anubhootipandey05@gmail.com>
2025-05-21 20:00:10 +0530

    interview questions

---
+++ Interview-Question/coding-question/11_code_0/script.js
@@ -0,0 +1,17 @@
+const promise = new Promise((res) => res(2));
+
+promise.then((v) => {
+    console.log(v);
+    return v*2;
+})
+.then((v) => {
+    console.log(v);
+    return v*2;
+})
+.finally((v) => {
+    console.log(v);
+    return 0;
+})
+.then((v) => {
+    console.log(v);
+});
---
+++ Interview-Question/coding-question/11_code_0/script1.js
@@ -0,0 +1,8 @@
+// setInterval(() => {
+//     console.log("This message repeats every 2 seconds!");
+//   }, 2000); 
+  
+  setTimeout(() => {
+    console.log("Hello after 3 seconds!");
+  }, 3000); 
+  
